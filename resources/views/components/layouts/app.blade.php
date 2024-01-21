ab128d1a98510a969995051f8a32cfd61cb42542
akbarhlubis <akbar_hamonangan_lubis@teknokrat.ac.id>
2024-01-21 07:35:38 +0700

    first commit

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>{{ $title ?? 'Page Title' }}</title>
+        @livewireStyles
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+
+    </head>
+    <body>
+        {{ $slot }}
+        @livewireScripts
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
